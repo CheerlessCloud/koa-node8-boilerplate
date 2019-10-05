059224fd906bdc406fbec62815dc70b8b4e7eb65
CheerlessCloud <nnsceg@gmail.com>
2019-10-05 19:43:57 +0300

    feat(http): add graceful shutdown

--- bin/http-server.js
+++ bin/http-server.js
@@ -1,10 +1,37 @@
-import httpServer from '../src/http/server';
+import gracefulShutdownMixin from 'http-shutdown';
+import httpServerApp from '../src/http/server';
 import conf from '../config';
 import logger from '../src/libs/logger';
 
-httpServer.listen(conf.get('httpPort'), () => {
+function onListen() {
   logger.info({
     message: 'HTTP server successfully ran',
     port: conf.get('httpPort'),
+  });
+}
+
+const nativeHttpServer = httpServerApp.listen(conf.get('httpPort'), onListen);
+const serverWithShutdown = gracefulShutdownMixin(nativeHttpServer);
+
+process.on('SIGINT', () => {
+  logger.info({ message: 'Start shutdown http server' });
+
+  const shutdownCallback = err => {
+    if (err) {
+      logger.error(err);
+      process.exit(1);
+    }
+
+    process.exit(0);
+  };
+
+  const timer = setTimeout(() => {
+    logger.warn({ message: 'Force shutdown http server by timeout' });
+    serverWithShutdown.forceShutdown(err => shutdownCallback(err));
+  }, conf.get('gracefulStopTimeout'));
+
+  serverWithShutdown.shutdown(err => {
+    clearTimeout(timer);
+    shutdownCallback(err);
   });
 });
